ea18b8f5e7eac5d96288f78291c826591cb129f1
oussemamansouri <oussemamansouri619@gmail.com>
2024-10-28 23:48:56 +0100

    Update database_manager.py

---
+++ database_manager.py
@@ -0,0 +1,12 @@
+import json
+
+def save_data(file_path, data):
+    with open(file_path, 'w') as file:
+        json.dump(data, file)
+
+def load_data(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
