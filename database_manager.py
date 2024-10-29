f448f8f1a6f0c5998fb410e42a39c47f36b88f76
oussemamansouri <oussemamansouri619@gmail.com>
2024-10-29 21:54:52 +0100

    Update database_manager.py

--- database_manager.py
+++ database_manager.py
@@ -2,11 +2,11 @@ import json
 
 def save_data(file_path, data):
     with open(file_path, 'w') as file:
-        json.dump(data, file)
+        json.dump(data, file, indent=4)
 
 def load_data(file_path):
     try:
         with open(file_path, 'r') as file:
             return json.load(file)
-    except FileNotFoundError:
-        return []
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {}  # Return an empty dictionary if file not found or empty
